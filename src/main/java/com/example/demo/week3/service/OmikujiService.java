2af27d90cd9d4b8fe9472665d62d3381406a971d
Apple <apple@appurunoMacBook.local>
2022-05-25 23:52:20 +0900

    omikuji完了しました

--- src/main/java/com/example/demo/week3/service/OmikujiService.java
+++ src/main/java/com/example/demo/week3/service/OmikujiService.java
@@ -1,7 +1,5 @@
 package com.example.demo.week3.service;
 
-import java.util.Random;
-
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +16,32 @@ public class OmikujiService {
      * @return result おみくじの結果
      */
     public String getResult() {
-
+    int luck = (int)(Math.random()*7);
+    String result ="";
+    switch(luck){
+    case 0:
+    	result = "大吉" ;
+    	break ;
+    case 1:
+    	result= "中吉";
+    	break ;
+    case 2:
+    	result ="小吉";
+    	break ;
+    case 3:
+    	result ="吉";
+    	break ;
+    case 4:
+    	result ="末吉";
+    	break ;
+    case 5:
+    	result ="凶";
+    	break ;
+    case ６:
+    	result ="大凶";
+    	break ;
+    }
+    	return result;   	
     }
 
     /**
@@ -35,8 +58,32 @@ public class OmikujiService {
      * @return resultComment おみくじ結果に対してのコメント
      */
     public String getResultComment(String result) {
-
+    String resultComment="";
+    switch(result){
+    case "大吉":
+    	resultComment="ランチに入ったお店でサービスしてもらえるかも";
+    	break ;
+    case "中吉":
+    	resultComment="風邪引かないかも";
+    	break ;
+    case "小吉":
+    	resultComment="なぜか髪サラサラになるかも";
+    	break ;
+    case "吉":
+    	resultComment="悩み事解決のヒントが見つかるかも";
+    	break ;
+    case "末吉":
+    	resultComment="買い物で小銭をぴったり出せるかも";
+    	break ;
+    case "凶":
+    	resultComment="コーヒー買うのに万札しかないかも";
+    	break ;
+    case "大凶":
+    	resultComment="コインパーキングで別車の清算しちゃうかも";
+    	break ;
+    }
+    		return resultComment ;
+}
+}
         //おみくじの結果に紐づいた、コメントを定数として定義
 
-    }
-}
