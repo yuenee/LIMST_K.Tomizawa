72763c0e2999d51c61e397fe9177b7272f20ea09
Apple <apple@appurunoMacBook.local>
2022-05-29 14:10:54 +0900

    omikuji完了しました

--- src/main/java/com/example/demo/week3/service/OmikujiService.java
+++ src/main/java/com/example/demo/week3/service/OmikujiService.java
@@ -17,32 +17,25 @@ public class OmikujiService {
      */
     public String getResult() {
     int luck = (int)(Math.random()*7);
-    String result ="";
+    String result = null ;
     switch(luck){
-    case 0:
-    	result = "大吉" ;
+    case 0: result = "大吉" ;
     	break ;
-    case 1:
-    	result= "中吉";
+    case 1: result= "中吉";
     	break ;
-    case 2:
-    	result ="小吉";
+    case 2: result ="小吉";
     	break ;
-    case 3:
-    	result ="吉";
+    case 3: result ="吉";
     	break ;
-    case 4:
-    	result ="末吉";
+    case 4: result ="末吉";
     	break ;
-    case 5:
-    	result ="凶";
+    case 5: result ="凶";
     	break ;
-    case ６:
-    	result ="大凶";
+    case ６: result ="大凶";
     	break ;
     }
     	return result;   	
-    }
+ }
 
     /**
      * <pre>
@@ -58,32 +51,25 @@ public class OmikujiService {
      * @return resultComment おみくじ結果に対してのコメント
      */
     public String getResultComment(String result) {
-    String resultComment="";
+    String resultComment = null;
     switch(result){
-    case "大吉":
-    	resultComment="ランチに入ったお店でサービスしてもらえるかも";
+    case "大吉": resultComment="ランチに入ったお店でサービスしてもらえるかも";
     	break ;
-    case "中吉":
-    	resultComment="風邪引かないかも";
+    case "中吉": resultComment="風邪引かないかも";
     	break ;
-    case "小吉":
-    	resultComment="なぜか髪サラサラになるかも";
+    case "小吉": resultComment="なぜか髪サラサラになるかも";
     	break ;
-    case "吉":
-    	resultComment="悩み事解決のヒントが見つかるかも";
+    case "吉" : resultComment="悩み事解決のヒントが見つかるかも";
     	break ;
-    case "末吉":
-    	resultComment="買い物で小銭をぴったり出せるかも";
+    case "末吉": resultComment="買い物で小銭をぴったり出せるかも";
     	break ;
-    case "凶":
-    	resultComment="コーヒー買うのに万札しかないかも";
+    case "凶": resultComment="コーヒー買うのに万札しかないかも";
     	break ;
-    case "大凶":
-    	resultComment="コインパーキングで別車の清算しちゃうかも";
+    case "大凶": resultComment="コインパーキングで別車の清算しちゃうかも";
     	break ;
     }
     		return resultComment ;
-}
+  }
 }
         //おみくじの結果に紐づいた、コメントを定数として定義
 
