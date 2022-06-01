2a30f06985e628d259fc1537ae188ad2ae708060
Apple <apple@appurunoMacBook.local>
2022-06-01 23:52:29 +0900

    nabeatu終戦しました

--- src/main/java/com/example/demo/week3/service/SingleNabeatuService.java
+++ src/main/java/com/example/demo/week3/service/SingleNabeatuService.java
@@ -5,30 +5,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class SingleNabeatuService {
 
-    /**
-     * <pre>
-     * 【問題文】
-     * isAhoNumberは、引数のinputNumber(入力値)がアホになる数値('3を含む数字'または'3の倍数')
-     * かどうかを判定するメソッドです。
-     * ・入力値がアホになる数値('3を含む数字'または'3の倍数')かどうかを判定する処理を記述してください
-     * ・判別結果は変数flgに格納して返却してください。
-     * ※アホになる数値の場合はtrue, アホにならない数値の場合はfalse
-     * </pre>
-     *
-     * @param inputNumber 画面で入力した数字
-     * @return flg アホになる('3を含む数字'または'3の倍数')かどうかの結果
-     */
-    public boolean isAhoNumber(int inputNumber) {
-    	boolean flg ;
-    	String kazu = String.valueOf(inputNumber);
-    	if((inputNumber%3==0)||(kazu.contains("3"))) {
-	    flg = true ;
-  }
-        else {
-	   flg =false ;
-  }
-       return flg ;
-    	}
+	/**
+	 * <pre>
+	 * 【問題文】
+	 * isAhoNumberは、引数のinputNumber(入力値)がアホになる数値('3を含む数字'または'3の倍数')
+	 * かどうかを判定するメソッドです。
+	 * ・入力値がアホになる数値('3を含む数字'または'3の倍数')かどうかを判定する処理を記述してください
+	 * ・判別結果は変数flgに格納して返却してください。
+	 * ※アホになる数値の場合はtrue, アホにならない数値の場合はfalse
+	 * </pre>
+	 *
+	 * @param inputNumber 画面で入力した数字
+	 * @return flg アホになる('3を含む数字'または'3の倍数')かどうかの結果
+	 */
+	public boolean isAhoNumber(int inputNumber) {
+		boolean flg;
+		String kazu = String.valueOf(inputNumber);
+		if ((inputNumber % 3 == 0) || (kazu.contains("3"))) {
+			flg = true;
+		} else {
+			flg = false;
+		}
+		return flg;
+	}
 }
-    
-
